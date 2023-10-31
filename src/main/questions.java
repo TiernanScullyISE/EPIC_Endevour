18860ee90428da45c4218b476659559a9d19b730
TiernanScullyISE <145368009+TiernanScullyISE@users.noreply.github.com>
2023-10-31 16:30:38 +0000

    test

--- src/main/questions.java
+++ src/main/questions.java
@@ -43,24 +43,24 @@ public class questions {
             "True or False: BNF stands for Backus-Naur Function",
             "True or False: Syntax in programming languages specifies the order of execution of statements, while semantics determines the structure of code",
             "True or False: Inductive reasoning is a method for proving statements by providing a counterexample to disprove them",
-            "True or False: An Ordered Binary Decision Diagram (OBDD) is the same as a Reduced Ordered BDD (ROBDD)",
-            "True or False: Congruence is an equivalence property that allows for the substitution of equivalent expressions in any context",
-            "True or False: BNF is a notation used to describe the syntax of programming languages, focusing on the structure and grammar of code"
+            "True or False: Caching is a technique used to improve memory access times in a computer system.",
+            "True or False: Cache memory is slower but larger in size compared to main memory (RAM).",
+            "True or False: The ALU (Arithmetic Logic Unit) is responsible for performing mathematical operations in the CPU."
         };
-        boolean[] intermediateAnswers = { true, true, true, true, false, false, false, true, true };
+        boolean[] intermediateAnswers = { true, true, true, true, false, false, true, false, true };
 
         String[] expertQuestions = {
             "True or False: The symmetric difference of two sets is always commutative",
             "True or False: A partial order is always antisymmetric",
             "True or False: In proof by contradiction, we assume the statement is true and derive a contradiction",
-            "True or False: A superscalar processor can execute multiple instructions simultaneously",
-            "True or False: The concept of \"endianess\" determines how multi-byte data is stored in memory",
-            "True or False: A digital signal processor (DSP) is specifically designed for complex scientific calculations",
-            "True or False: A GPU (Graphics Processing Unit) is typically designed for parallel processing of graphics and general-purpose computation",
-            "True or False: Quicksort is generally faster than mergesort for small lists but can be less efficient for large lists",
-            "True or False: An NP-complete problem is a problem for which a proposed solution can be verified quickly but for which there is no known quick method to find a solution"
+            "True or False: An Ordered Binary Decision Diagram (OBDD) is the same as a Reduced Ordered BDD (ROBDD)",
+            "True or False: Congruence is an equivalence property that allows for the substitution of equivalent expressions in any context.",
+            "True or False: BNF is a notation used to describe the syntax of programming languages, focusing on the structure and grammar of code.",
+            "True or False: The precision of a 3 digit decimal number from 000 to 999 is 1",
+            "True or False: The error of a 3 digit decimal number from 000 to 999 is 0.5.",
+            "True or False: True or False: The radix or base of a number system defines the range of possible values that a digit may have"
         };
-        boolean[] expertAnswers = { true, true, true, true, true, false, true, true, true };
+        boolean[] expertAnswers = { true, true, true, false, true, true, true, true, true };
 
         String[] selectedCategoryQuestions;
         boolean[] selectedCategoryAnswers;
@@ -128,151 +128,7 @@ public class questions {
             }
         }
         
-<<<<<<< HEAD
-        ArrayList<String> allCSciList = new ArrayList<>(); // Create an ArrayList from the original array so i can remove elements from the list 
-        for (String item : allCSci) {
-            allCSciList.add(item);
-        }
-        
-        ArrayList<String> allCOrgList = new ArrayList<>(); // Create an ArrayList from the original array so i can remove elements from the list 
-        for (String item : allCOrg) {
-            allCOrgList.add(item);
-        }
-        
-		Random random = new Random();
-		
-		for (int i = 0; i < 6; i++) { // loops 6 times because only need 6 questions
-			int randIndex = random.nextInt(allDMList.size()); // the random index is a number between 0 and the length/size of allDM (9)
-			questionsDM[i] = allDMList.get(randIndex); // add that index to questionsDM
-			answersDM[i] = allDMAns[randIndex]; // add that index to answersDM
-			allDMList.remove(randIndex); // remove the random index from the list because i don't want to pick it again
-			// System.out.println(questionsDM[i]);
-		} 
 
-		for (int i = 0; i < 6; i++) { // loops 6 times because only need 6 questions
-			int randIndex = random.nextInt(allCSciList.size()); // the random index is a number between 0 and the length/size (9)
-			questionsCSci[i] = allCSciList.get(randIndex); // add that index to questionsDM
-			answersCSci[i] = allCSciAns[randIndex]; // add that index to answersDM
-			allCSciList.remove(randIndex); // remove the random index from the list because i don't want to pick it again
-			// System.out.println(questionsCSci[i]);
-		} 
-		
-		for (int i = 0; i < 6; i++) { // loops 6 times because only need 6 questions
-			int randIndex = random.nextInt(allCOrgList.size()); // the random index is a number between 0 and the length/size (9)
-			questionsCOrg[i] = allCOrgList.get(randIndex); // add that index to questionsDM
-			answersCOrg[i] = allCOrgAns[randIndex]; // add that index to answersDM
-			allCOrgList.remove(randIndex); // remove the random index from the list because i don't want to pick it again
-			// System.out.println(questionsCOrg[i]);
-		} 
-		
-////////////////////////////////////////////////
-		
-		int correct = 0, score = 0;
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Welcome to the quiz. There are 3 different difficutly questions and each of them are worth a different amount of points, \nBeginner gives 1, Intermediate gives 3 and Expert gives 5. \nPress Enter to start.");
-        scanner.nextLine(); // once they press enter the timer starts
-
-        long startTime = System.currentTimeMillis(); // gets the time so when quiz over it can take it away from the time it is when they finish to get elapsed time
-		
-		System.out.println("Answer the following 6 questions on Discrete Maths:");
-		for (int i = 0; i < 6; i++) {
-			System.out.println(questionsDM[i]);
-			boolean answer = scanner.nextBoolean();
-			if (answer == answersDM[i]) {
-				correct++;
-				System.out.println("Correct, you have answered "+correct+" questions correctly");
-				for (int j = 0; j < questionsDM.length; j++) { // check every index in questionsDM and if that element is in 
-				    String question = questionsDM[i];			// either expertDM, intermediateDM or beginnerDM then you get score
-				    if (isInArray(question, expertDM)) { 
-				        score += 5; // expert gives 5 score
-				    } else if (isInArray(question, intermediateDM)) {
-				        score += 3; // intermediate gives 3 score
-				    } else {
-				        score += 1; // beginner gives 1 score
-				    }
-				    // System.out.println(score); testing if the score counter works as intended
-				    break;
-				}
-			} else {
-				System.out.println("Incorrect, the answer was: "+answersDM[i]);
-			}
-		}
-		
-		System.out.println("Answer the following 6 questions on Computer Science:");
-		for (int i = 0; i < 6; i++) {
-			System.out.println(questionsCSci[i]);
-			boolean answer = scanner.nextBoolean();
-			if (answer == answersCSci[i]) {
-				correct++;
-				System.out.println("Correct, you have answered "+correct+" questions correctly");
-				for (int j = 0; j < questionsCSci.length; j++) {
-				    String question = questionsCSci[i];
-				    if (isInArray(question, expertCSci)) {
-				        score += 5;
-				    } else if (isInArray(question, intermediateCSci)) {
-				        score += 3;
-				    } else {
-				        score += 1;
-				    }
-				    break;
-				}
-			} else {
-				System.out.println("Incorrect, the answer was: "+answersCSci[i]);
-			}
-		}
-		
-		System.out.println("Answer the following 6 questions on Computer Organisation:");
-		for (int i = 0; i < 6; i++) {
-			System.out.println(questionsCOrg[i]);
-			boolean answer = scanner.nextBoolean();
-			if (answer == answersCOrg[i]) {
-				correct++;
-				System.out.println("Correct, you have answered "+correct+" questions correctly");
-				for (int j = 0; j < questionsCOrg.length; j++) {
-				    String question = questionsCOrg[i];
-				    if (isInArray(question, expertCOrg)) {
-				        score += 5;
-				    } else if (isInArray(question, intermediateCOrg)) {
-				        score += 3;
-				    } else {
-				        score += 1;
-				    }
-				    break;
-				}
-			} else {
-				System.out.println("Incorrect, the answer was: "+answersCOrg[i]);
-			}
-		}
-		
-        long endTime = System.currentTimeMillis(); // get time they finish the quiz
-        long elapsedTime = endTime - startTime; // end - start = elapsed
-        long seconds = elapsedTime / 1000; // converts from miliseconds to seconds
-        // System.out.println(seconds);
-        long minutes = seconds / 60; // converts from seconds to minutes
-        seconds %= 60; // get the modulus to get the amount of left over seconds
-        
-		System.out.println("The quiz is over, you answered "+correct+" questions correctly out of 18");
-		double percent = (correct/18.0)*100;
-		System.out.println("You got "+percent+"%");
-		System.out.println("Quiz completed in " + minutes + " minutes and " + seconds + " seconds.");
-		System.out.println("You got a total score of "+score);
-	
-	
-}	
-
-	// method to check if a question is in an array
-	private static boolean isInArray(String question, String[] array) {
-	    for (String item : array) {
-	        if (item.equals(question)) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}
-
-=======
         long endTime = System.currentTimeMillis(); // get time they finish the quiz
 		long elapsedTime = endTime - startTime; // end - start = elapsed
 		long seconds = elapsedTime / 1000; // converts from miliseconds to seconds
@@ -311,5 +167,5 @@ public class questions {
         }
         return newArray;
     }
->>>>>>> development
+
 }
